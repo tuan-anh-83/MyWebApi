19afb5c3a448d7382af38294208aa1be075470e4
tuan-anh-83 <117378521+tuan-anh-83@users.noreply.github.com>
2024-01-29 11:02:21 +0700

    Add project files.

---
+++ MyWebApi/Model/HangHoa.cs
@@ -0,0 +1,13 @@
+﻿namespace MyWebApi.Model
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+     }
+}
